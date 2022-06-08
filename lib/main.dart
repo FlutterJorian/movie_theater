1182b3db2e444e28db1c8949b363e3ac3868222e
Jorian van der Kolk <jorian@iconica.nl>
2022-06-08 09:05:02 +0200

    changed readme

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,7 @@ import 'package:movie_theater/screens/buy_ticket/buy_ticket.dart';
 import 'package:movie_theater/screens/select_movie/select_movie.dart';
 
 void main() {
-  // Flutter 3 bug (https://github.com/flutter/flutter/issues/101007) temp fix:
-  WidgetsFlutterBinding.ensureInitialized();
-  Future.delayed(const Duration(milliseconds: 200), () {
-    runApp(const MovieTheater());
-  });
+  runApp(const MovieTheater());
 }
 
 class MovieTheaterRoute {
